6264d7591a948fb97125274e38520dd8a4531922
Markus Vrancea <ge54jad@tum.de>
2023-11-28 17:52:32 +0100

    implemented FileReader for the XML Files and used this in the main function to run the Simulation

---
+++ src/inputOutput/inputReader/XMLCuboidReader.cpp
@@ -0,0 +1,44 @@
+//
+// Created by markus on 11/26/23.
+//
+
+#include "XMLCuboidReader.h"
+#include "xsd/Simulation.hxx"
+#include "iostream"
+
+XMLCuboidReader::XMLCuboidReader() = default;
+
+XMLCuboidReader::~XMLCuboidReader() = default;
+
+std::vector<CuboidParticleGenerator> XMLCuboidReader::readFile(const char *filename) {
+
+    std::vector<CuboidParticleGenerator> generators;
+    std::array<double, 3> x;
+    std::array<double, 3> v;
+    double m;
+    std::array<int, 3> N;
+    double spacing;
+    int type;
+
+    //std::cout << "Trying to parse with: " + filename << std::endl;
+
+    std::unique_ptr<simulation> parameters = simulation_(filename);
+
+    //std::cout << "End reached\n" << std::endl;
+
+    auto cuboids = parameters->cuboid();
+
+    for(auto c: cuboids){
+
+        x = {c.position().x(), c.position().y(), c.position().z()};
+        v = {c.velocity().v(), c.velocity().w(), c.velocity().z()};
+        m = c.mass();
+        N = {c.grid().Nx(), c.grid().Ny(), c.grid().Nz()};
+        spacing = c.spacing();
+        type = c.type();
+
+        generators.emplace_back(N, spacing, m, v, x, type);
+    }
+
+    return generators;
+}
