db6014093915aeff7cf5e63dc47c02fde20ca5c7
laylazadina <layla.zadina@gmail.com>
2023-12-13 18:03:22 +0100

    implement fucntions in Thermostat

--- src/Thermostat.cpp
+++ src/Thermostat.cpp
@@ -4,7 +4,8 @@
 #include "Containers/LinkedCellContainer2.h"
 #include "Thermostat.h"
 #include <cmath>
-#include <algorithm>
+#include <array>
+#include "utils/MaxwellBoltzmannDistribution.h"
 
 // Constructor
 Thermostat::Thermostat(double Tinit, double Ttarget, double deltaT, int nthermostat, bool useBrownianMotion)
@@ -13,6 +14,8 @@ Thermostat::Thermostat(double Tinit, double Ttarget, double deltaT, int nthermos
 /** Apply the thermostat to a LinkedCellContainer2 */
 void Thermostat::applyThermostat(LinkedCellContainer2& particleContainer, int currentStep) {
     if (currentStep % nthermostat == 0) {
+        /** Systems that have no initial velocities need to be initialized
+         * with Brownian Motion to have a non-zero temperature. */
         if (useBrownianMotion) {
             initializeWithBrownianMotion(particleContainer);
         }
@@ -25,34 +28,52 @@ void Thermostat::applyThermostat(LinkedCellContainer2& particleContainer, int cu
     }
 }
 
-/** Calculate the current temperature of the system */
-double Thermostat::calculateCurrentTemperature(const LinkedCellContainer2& particleContainer) const {
+/** Helper function
+ * Calculate the current temperature of the system */
+double Thermostat::calculateCurrentTemperature(LinkedCellContainer2& particleContainer) const {
     double Ekin = 0.0;
     // apply formula (2) from the worksheet to calculate the Kinetic Energy of the particles
-    const auto& particles = particleContainer.getParticles();
-    for (const auto& particle : particles) {
-        double vSquared = std::inner_product(particle.getV().begin(), particle.getV().end(), particle.getV().begin(), 0.0);
-        Ekin += particle.getM() * vSquared / 2.0;
-    }
-    //apply formula (1) from the worksheet to calculate T 
-    return (2 * Ekin) / (3 * particles.size()); // Assuming 3 dimensions
-}
-
-// Initialize particles' velocities with Brownian motion
-void Thermostat::initializeWithBrownianMotion(LinkedCellContainer2& particleContainer) {
     auto& particles = particleContainer.getParticles();
     for (auto& particle : particles) {
-        double fi = std::sqrt(Tinit / particle.getM());
-        // Apply Maxwell-Boltzmann distribution scaling here
+        double vSquared = 0.0;
+        for (auto velocity : particle.getV()) {
+            vSquared += velocity * velocity;
+        }
+
+        Ekin += particle.getM() * vSquared / 2.0;
     }
+    //apply formula (1) from the worksheet to calculate T
+    return (2 * Ekin) / (numDimensions * kBoltzmann * particles.size());
 }
 
-// Scale the velocities of the particles
+
+/** Helper function
+ * Initialize particles' velocities with Brownian motion */
+void Thermostat::initializeWithBrownianMotion(LinkedCellContainer2& particleContainer) {
+        auto& particles = particleContainer.getParticles();
+        for (auto& particle : particles) {
+            auto bmVelocity = maxwellBoltzmannDistributedVelocity(std::sqrt(Tinit / particle.getM()), 3);
+            particle.setV(bmVelocity);
+        }
+
+}
+
+
+
+/** Helper function
+ * Scale the velocities of the particles */
 void Thermostat::scaleVelocities(LinkedCellContainer2& particleContainer, double scalingFactor) {
     auto& particles = particleContainer.getParticles();
     for (auto& particle : particles) {
-        auto& v = particle.getV();
-        std::transform(v.begin(), v.end(), v.begin(), [scalingFactor](double velocity) { return velocity * scalingFactor; });
+        const auto& currentVelocity = particle.getV();
+        std::array<double, 3> scaledVelocity;
+
+        for (size_t i = 0; i < currentVelocity.size(); ++i) {
+            scaledVelocity[i] = currentVelocity[i] * scalingFactor;
+        }
+        
+        particle.setV(scaledVelocity);
     }
 }
+
 
--- src/Thermostat.h
+++ src/Thermostat.h
@@ -13,10 +13,15 @@
 
 class Thermostat {
 private:
+    static constexpr int numDimensions = 3;
+    static constexpr double kBoltzmann = 1.0;
     double Tinit;          // Initial temperature
     double Ttarget;        // Target temperature
     double deltaT;         // Max temperature change per application
     int nthermostat;       // Number of time steps for periodic application
+
+    /** useBrownianMotion = True if initial Velocities == 0, False otherwise
+     * initialize Thermostat with this flag*/
     bool useBrownianMotion;// Flag for initializing with Brownian Motion
 
 public:
@@ -27,7 +32,7 @@ public:
     void applyThermostat(LinkedCellContainer2& particleContainer, int currentStep);
 
     /** Helper method to calculate the current temperature of the system */
-    double calculateCurrentTemperature(const LinkedCellContainer2& particleContainer) const;
+    double calculateCurrentTemperature(LinkedCellContainer2& particleContainer) const;
 
     /** Function to initialize particles' velocities with Brownian motion */
     void initializeWithBrownianMotion(LinkedCellContainer2& particleContainer);
